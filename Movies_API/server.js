fd06b22f079e5efb490c1cd2a106092ecb358687
boopathirajan <boopathirajan29102003@gmail.com>
2023-10-25 18:23:39 +0530

    25/10/2023

---
+++ Movies_API/server.js
@@ -0,0 +1,39 @@
+const http = require('http')
+
+const getReq=require('./methods/get-request')
+const postReq=require('./methods/post-request')
+const putReq=require('./methods/put-request')
+const deleteReq=require('./methods/delete-request')
+
+let movies=require()
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3600;
+
+const server = http.createServer((req, res) => {
+    req.movies=movies;
+    switch (req.method) {
+        case "GET":
+            getReq(req, res);
+            break;
+        case "POST":
+            postReq(req, res);
+            break;
+        case "PUT":
+            putReq(req, res);
+            break;
+        case "DELETE":
+            deleteReq(req, res);
+            break;
+        default:
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "application/json");
+            res.write(JSON.stringify({ message: "page NOT found", name: "Boopathi" }));
+            res.end()
+    }
+
+});
+
+server.listen(PORT, () => {
+    console.log(`server listening on ${PORT}`)
+})
